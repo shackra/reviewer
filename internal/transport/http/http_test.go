5e693067a5335c1e663e52cf3495a53128755e1f
Jorge Javier Araya Navarro <jorge@esavara.cr>
2023-12-27 22:58:20 -0600

    Fix test of transport

--- internal/transport/http/http_test.go
+++ internal/transport/http/http_test.go
@@ -36,7 +36,7 @@ func TestListProducts(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	expected := `{"Products":[],"NextPage":""}`
+	expected := `{"Products":null,"NextPage":null}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
